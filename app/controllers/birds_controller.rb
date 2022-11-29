2bca5b7a48b3506236c71f4c6c732fcf96651bd8
JasonKiptoo <kiptoojason@gmail.com>
2022-11-30 00:23:31 +0300

    Completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -21,11 +21,57 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+  # def update
+  #   bird= Bird.find_by(id: params[:id])
+  #   if bird
+  #   bird= Bird.update(bird_params)
+  #   render json: bird
+  #   else
+  #     render json: {error: "bird not found"}, status: :not_found
+  #   end
+
+    def update
+      bird = Bird.find_by(id: params[:id])
+      if bird
+        bird.update(bird_params)
+        render json: bird
+      else
+        render json: { error: "Bird not found" }, status: :not_found
+      end
+    end
+
+
+    def increment_likes
+      bird = Bird.find_by(id: params[:id])
+      if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+      else
+        render json: { error: "Bird not found" }, status: :not_found
+  end
+
+    end
+
+
+    def destroy
+  bird = Bird.find_by(id: params[:id])
+
+      if bird
+
+        bird.destroy
+        head :no_content
+        
+      else
+
+        render json: {error: "Bird not found"}, status: :not_found
+      end
+    end
+
 
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
